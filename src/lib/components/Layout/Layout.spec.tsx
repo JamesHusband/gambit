9bb7cad163a274bd09f2c37f2d8a7cf7ed7211f6
James Husband <jchusband@proton.me>
2025-07-08 12:23:14 +0100

    feat(comp): Add layout wrapper

--- src/lib/components/Layout/Layout.spec.tsx
+++ src/lib/components/Layout/Layout.spec.tsx
@@ -2,8 +2,9 @@ import { render, screen } from "@testing-library/react";
 import { Layout } from "./Layout";
 
 describe("Layout", () => {
-  it("should render", () => {
-    render(<Layout>Test</Layout>);
+  it("should render with children", () => {
+    const { container } = render(<Layout>Test</Layout>);
     expect(screen.getByText("Test")).toBeInTheDocument();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
