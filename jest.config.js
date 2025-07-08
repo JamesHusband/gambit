b146dc9cb2d050c1c96df59e44911d54de7049cc
James Husband <jchusband@proton.me>
2025-07-08 11:53:23 +0100

    fix(jest): Jest config for jsx

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,13 @@ export default {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+        useESM: true,
+      },
+    ],
   },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
 };
