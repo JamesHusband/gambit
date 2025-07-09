ef46f56389c37ebeff6dc5c5acd350741ec83fa4
James Husband <jchusband@proton.me>
2025-07-09 15:54:04 +0100

    refac(hook):  declare rank / file in state

--- src/lib/hooks/useChessBoard/useChessBoard.spec.ts
+++ src/lib/hooks/useChessBoard/useChessBoard.spec.ts
@@ -12,4 +12,25 @@ describe("useChessBoard", () => {
     renderHook(() => useChessBoard());
     expect(mockInitializeBoard).toHaveBeenCalled();
   });
+  it("returns the board", () => {
+    const { result } = renderHook(() => useChessBoard());
+    expect(result.current.board).toEqual(mockInitializeBoard());
+  });
+  it("returns the files", () => {
+    const { result } = renderHook(() => useChessBoard());
+    expect(result.current.files).toEqual([
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+    ]);
+  });
+  it("returns the ranks", () => {
+    const { result } = renderHook(() => useChessBoard());
+    expect(result.current.ranks).toEqual([8, 7, 6, 5, 4, 3, 2, 1]);
+  });
 });
