50606776be39750a553fb91ae6029d903f8bf619
James Husband <jchusband@proton.me>
2025-07-09 15:54:39 +0100

    refac(util): import rank / file from state

--- src/lib/utils/initializeBoard/initializeBoard.ts
+++ src/lib/utils/initializeBoard/initializeBoard.ts
@@ -1,9 +1,12 @@
 import type { Square } from "../../types";
 
-export const initializeBoard = (): Square[][] => {
-  const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
-
+export const initializeBoard = ({
+  ranks,
+  files,
+}: {
+  ranks: number[];
+  files: string[];
+}): Square[][] => {
   return ranks.map((rank) =>
     files.map((file) => ({
       position: `${file}${rank}`,
